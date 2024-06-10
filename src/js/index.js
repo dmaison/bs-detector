d520742fd62cd5b2950c27dd2cc56cdd06577e91
dmaison <davemaison@gmail.com>
2024-06-10 17:13:42 -0400

    stopping point

--- src/js/index.js
+++ src/js/index.js
@@ -51,6 +51,7 @@ const WORDS_EMPHASIZE = [
  * @type {array}
  */
 const WORDS_SUGGESTIVE = [
+    "actually",
     "extremely",
     "cataclysmic",
     "chaotic",
@@ -65,6 +66,7 @@ const WORDS_SUGGESTIVE = [
     "purely",
     "really",
     "remarkable",
+    "restrictive",
     "shameful",
     "slight", 
     "truly",
